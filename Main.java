ab5fa86a8f3a3fa87524cd46af88f80911744de4
Cristina Cotetiu <cristina.badescu@softvision.ro>
2021-08-31 10:20:10 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Random;
+
+import static com.company.CristinaCotetiuWeek1Homework.*;
+import static com.company.CristinaCotetiuWeek2Homework.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Week1 Homework:");
+        exercise4();
+        exercise5();
+        exercise6();
+        exercise7();
+
+        System.out.println("Week2 Homework:");
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4w2();
+    }
+}
